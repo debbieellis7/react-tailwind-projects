faab4964d7c0637e818e94ad19b8e03fc8ae2e29
debbieellis7 <1debbieellis@gmail.com>
2024-10-10 08:44:17 +0800

    indoor-plants - react & navbar setup

---
+++ indoor-plants/src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+import "remixicon/fonts/remixicon.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
